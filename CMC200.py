4d0c6cbb9c9c6e7a725f61f8c15b93163366df21
GaspardLeMarque <gleb.ushakov@yahoo.com>
2020-11-25 00:25:41 +0100

    Add EDA

--- CMC200.py
+++ CMC200.py
@@ -1,10 +1,11 @@
-#Use pandas_datareader lib to get CMC200 index
+#Script to get CMC200 index
 from pandas_datareader import data 
 from pandas_datareader._utils import RemoteDataError
 import pandas as pd 
 from datetime import datetime 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-#Check the start date manually
 start_date = '2018-12-31'
 end_date = str(datetime.now().strftime('%Y-%m-%d'))
 
@@ -14,8 +15,39 @@ def GetData(ticker):
     try:
         stock_data = data.DataReader(ticker, 'yahoo', start_date, end_date)
         print(stock_data)
+        stock_data.to_csv('CMC200.csv') 
 
     except RemoteDataError:
         print('No data for {t}'.format(t=ticker))
 
 GetData(my_ticker)
+
+#Data and EDA
+df = pd.read_csv("CMC200.csv", index_col='Date') 
+
+#Plot the Adj Close price
+df['Adj Close'].plot(label='CMC200', legend=True)
+plt.show()  
+
+#Histogram of the daily price change percent of Adj Close price
+df['Adj Close'].pct_change().plot.hist(bins=100)
+plt.xlabel('Adjusted close 1-day percent change')
+plt.show()
+
+#Check for autocorrelation
+#Create 5-day % changes of Adj Close for the current day, and 5 days in the future
+df['5d_future_close'] = df['Adj Close'].shift(-5)
+df['5d_close_future_pct'] = df['5d_future_close'].pct_change(5)
+df['5d_close_pct'] = df['Adj Close'].pct_change(5)
+
+#Calculate the correlation matrix between the 5d close pecentage changes (current and future)
+corr = df[['5d_close_pct', '5d_close_future_pct']].corr()
+print(corr) #No autocorrelation
+
+#Scatter plot of the current 5-day percent change vs the future 5-day percent change
+plt.scatter(df['5d_close_pct'], df['5d_close_future_pct'])
+plt.show() #No autocorrelation
+
+#Split into repeatable train and test sets
+df_train, df_test = train_test_split(df['Adj Close'], test_size=0.33, random_state=123)
+print(df_train.shape, df_test.shape)
