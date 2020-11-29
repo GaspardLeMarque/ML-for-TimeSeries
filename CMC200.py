d01d34abaf068774b94091423ad90bbb8ad98486
GaspardLeMarque <gleb.ushakov@yahoo.com>
2020-11-29 23:40:30 +0100

    Add more EDA with Seaborn

--- CMC200.py
+++ CMC200.py
@@ -4,6 +4,7 @@ from pandas_datareader._utils import RemoteDataError
 import pandas as pd 
 from datetime import datetime 
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 
 start_date = '2018-12-31'
@@ -34,19 +35,29 @@ df['Adj Close'].pct_change().plot.hist(bins=100)
 plt.xlabel('Adjusted close 1-day percent change')
 plt.show()
 
-#Check for autocorrelation
-#Create 5-day % changes of Adj Close for the current day, and 5 days in the future
+#Kernel Density Estimation
+sns.kdeplot(df['Adj Close'], shade=True)
+
+#Correlations
+#Calculate returns for the current day and for 5 days in the future
 df['5d_future_close'] = df['Adj Close'].shift(-5)
 df['5d_close_future_pct'] = df['5d_future_close'].pct_change(5)
 df['5d_close_pct'] = df['Adj Close'].pct_change(5)
 
-#Calculate the correlation matrix between the 5d close pecentage changes (current and future)
+#Calculate the correlation matrix between returns
 corr = df[['5d_close_pct', '5d_close_future_pct']].corr()
 print(corr) #No autocorrelation
 
 #Scatter plot of the current 5-day percent change vs the future 5-day percent change
 plt.scatter(df['5d_close_pct'], df['5d_close_future_pct'])
 plt.show() #No autocorrelation
+
+#Create a pairplot 
+sns.pairplot(df, vars=['5d_close_pct', '5d_close_future_pct'], diag_kind = 'kde', 
+             plot_kws = {'alpha': 0.8, 's': 80, 'edgecolor': 'k'})
+
+#Regression plot
+sns.regplot('5d_close_pct', '5d_close_future_pct', df) #No autocorrelation
 
 #Split into repeatable train and test sets
 df_train, df_test = train_test_split(df['Adj Close'], test_size=0.33, random_state=123)
