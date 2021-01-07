f8380ec0edb966f298617c2bc5345104c7c5960a
GaspardLeMarque <gleb.ushakov@yahoo.com>
2021-01-07 15:16:20 +0100

    Add train_the_model section

--- RNN_CMC.py
+++ RNN_CMC.py
@@ -36,6 +36,11 @@ GetData(my_ticker)
 
 df = pd.read_csv("CMC200.csv", index_col='Date') 
 
+#Define the device
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device
+
+
 #Setting the seed
 np.random.seed(123)
 torch.manual_seed(123)
@@ -54,7 +59,7 @@ scaler = scaler.fit(np.expand_dims(df_train, axis=1))
 train_data = scaler.transform(np.expand_dims(df_train, axis=1))
 test_data = scaler.transform(np.expand_dims(df_test, axis=1))
 
-#Divide TS to the sequences
+#Divide TS to the sequences for optimization
 def create_sequences(data, seq_length):
     xs = []
     ys = []
@@ -65,7 +70,7 @@ def create_sequences(data, seq_length):
         ys.append(y)
     return np.array(xs), np.array(ys)
 
-#Each sequence contains 5 data entries 
+#Each sequence contains 5 data entries  
 seq_length = 5
 X_train, y_train = create_sequences(train_data, seq_length)
 X_test, y_test = create_sequences(test_data, seq_length)
@@ -98,7 +103,7 @@ class PricePredictor(nn.Module):
 #Reset the state after each example (stateless LSTM)
   def reset_hidden_state(self):
     self.hidden = (
-        torch.zeros(self.n_layers, self.seq_len, self.n_hidden),
+        torch.zeros(self.n_layers, self.seq_len, self.n_hidden), #tensor filled w zeros
         torch.zeros(self.n_layers, self.seq_len, self.n_hidden)
     )
 #Pass sequences into the LSTM layers 
@@ -111,4 +116,62 @@ class PricePredictor(nn.Module):
       lstm_out.view(self.seq_len, len(sequences), self.n_hidden)[-1]
     y_pred = self.linear(last_time_step)
     return y_pred
-    
+
+#Train the model
+def train_model(
+  model, 
+  train_data, 
+  train_labels, 
+  test_data=None, 
+  test_labels=None
+):
+  loss_func = torch.nn.MSELoss(reduction='sum')
+
+  optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
+  num_epochs = 60
+
+  train_hist = np.zeros(num_epochs)
+  test_hist = np.zeros(num_epochs)
+
+  for t in range(num_epochs):
+    model.reset_hidden_state()
+    y_pred = model(X_train)
+    loss = loss_func(y_pred.float(), y_train)
+
+    if test_data is not None:
+      with torch.no_grad():
+        y_test_pred = model(X_test)
+        test_loss = loss_func(y_test_pred.float(), y_test)
+      test_hist[t] = test_loss.item()
+
+      if t % 10 == 0:  
+        print(f'Epoch {t} train loss: {loss.item()} test loss: {test_loss.item()}')
+    elif t % 10 == 0:
+      print(f'Epoch {t} train loss: {loss.item()}')
+
+    train_hist[t] = loss.item()
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+  
+  return model.eval(), train_hist, test_hist
+
+#Create the instance of the model
+model = PricePredictor(
+  n_features=1, 
+  n_hidden=512, 
+  seq_len=seq_length, 
+  n_layers=2
+)
+model, train_hist, test_hist = train_model(
+  model, 
+  X_train, 
+  y_train, 
+  X_test, 
+  y_test
+) #Losses are too high, need to change the model
+
+#Plot train and test losses
+plt.plot(train_hist, label="Training loss")
+plt.plot(test_hist, label="Test loss")
+plt.legend();
